d92b95247c49a897e2ddf1c69fb85454fbe20997
johndevops1995 <jfrodrizuga@gmail.com>
2023-07-28 13:39:46 -0500

    var let y const

---
+++ src/es6/00-let-const.js
@@ -0,0 +1,25 @@
+
+
+console.log(lastName);
+
+let fruit = 'apple';
+fruit = 'kiwi';
+console.log(fruit);
+
+const animal = 'Dog';
+animal = 'Cat';
+console.log(animal)
+
+
+const fruits = () => {
+    if(true){
+        var fruit1 = 'apple'; // function scope
+        let fruit2 = 'kiwi'; // block scope
+        const fruit3 = 'banana'; // block scope
+    }
+    console.log(fruit1);
+    console.log(fruit2);
+    console.log(fruit3);
+}
+
+fruits();    
