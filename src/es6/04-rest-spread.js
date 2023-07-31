14a363d7a6fd6e1d28fbda81b03f36793c8c63aa
johndevops1995 <jfrodrizuga@gmail.com>
2023-07-31 10:21:29 -0500

    spread operator and rest

--- src/es6/04-rest-spread.js
+++ src/es6/04-rest-spread.js
@@ -11,4 +11,48 @@ let {username, age } = user;
 console.log(username, user.age);
 
 
+// spread operator
+
+let person = { name: 'John', age: 28};
+let country = 'CO';
+
+let data = {id: 1, ...person, country };
+console.log(data);
+
+// rest
+
+function sum(num, ...values) {
+    console.log(values);
+    console.log(num + values[0]);
+    return num + values[0];
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
