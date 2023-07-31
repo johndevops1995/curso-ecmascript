9d99cda4025da3c3ea18d6ed5b9561e26fca5862
johndevops1995 <jfrodrizuga@gmail.com>
2023-07-31 09:00:10 -0500

    arrays destructuring y objets destructuring

---
+++ src/es6/04-rest-spread.js
@@ -0,0 +1,14 @@
+// arrays destructuring
+
+let fruits = ['Apple','Banana'];
+let [a, b] = fruits;
+console.log(a, fruits[1]);
+
+// object destructuring
+
+let user = {username: 'John', age: 43 };
+let {username, age } = user;
+console.log(username, age);
+
+
+
