42f57dd418a9fb5f3bdcf37a578fb788c89fced3
johndevops1995 <jfrodrizuga@gmail.com>
2023-07-31 08:33:16 -0500

    parametros por depefcto

---
+++ src/es6/default-params.js
@@ -0,0 +1,13 @@
+function newUser(name, age, country){
+    var name = name || 'John';
+    var age = age || 43;
+    var country = country || 'Colombia'
+    console.log(name, age, country);
+}
+
+newUser();
+newUser('David', 15, 'CO');
+
+function newAdmin(name = 'John', age = 43, country = 'CO'){
+    console.log(name, age, country);
+}
