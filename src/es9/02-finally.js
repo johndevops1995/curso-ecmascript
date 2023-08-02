ee41692ff1e09064193fdd864f762e978929b3e6
johndevops1995 <jfrodrizuga@gmail.com>
2023-08-02 11:42:29 -0500

    async

---
+++ src/es9/02-finally.js
@@ -0,0 +1,14 @@
+const anotherFuntion = () => {
+    return new Promise((resolve, reject)=> {
+        if (false) {
+            resolve('hey!!');
+        } else {
+            reject('whooooops!');
+        }         
+    })
+}
+
+anotherFuntion()
+  .then(response => console.log(response))
+  .catch(err => console.log(err))
+  .finally(() => console.log('Finnaly'));
