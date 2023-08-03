13253f5b839be909430931c733e7b393cbc68990
johndevops1995 <jfrodrizuga@gmail.com>
2023-08-03 09:38:08 -0500

    Promisse.finally

--- src/es9/03-async.js
+++ src/es9/03-async.js
@@ -1,4 +1,4 @@
-async function* anotherGenerator() {
+ async function* anotherGenerator() {
     yield await Promise.resolve(1);
     yield await Promise.resolve(2);
     yield await Promise.resolve(3);
