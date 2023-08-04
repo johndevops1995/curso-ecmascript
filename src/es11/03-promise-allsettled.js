6097bf2bcce54578ecd6c76bf19835e8bb6f59ab
johndevops1995 <jfrodrizuga@gmail.com>
2023-08-04 10:22:58 -0500

    Promise.all

--- src/es11/03-promise-allsettled.js
+++ src/es11/03-promise-allsettled.js
@@ -1,6 +1,6 @@
-const promise1 = new Promise((resolve, reject) => reject("Reject"));
+const promise1 = new Promise((resolve, reject) => reject("reject"));
 const promise2 = new Promise((resolve, reject) => resolve("resolve"));
-const promise3 = new Promise((resolve, reject) => resolve("Resolve2"));
+const promise3 = new Promise((resolve, reject) => resolve("resolve2"));
 
 Promise.allSettled([promise1, promise2, promise3])
-  .then(response => console.log(response));
+.then(response => console.log(response));
